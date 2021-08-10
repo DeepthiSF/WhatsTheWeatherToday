ab1506af2501604a65bfd665f7b5faf5bf231e3c
Deepthi <deepthi.gaddipati.f2jn@statefarm.com>
2021-08-10 09:09:32 -0500

    Added some code for temp conversion

--- CToF.js
+++ CToF.js
@@ -1,5 +1,9 @@
 
 //var apiKey = "6e06812aeffe12f5ac01e38127f2c4fe";
+var locationname = document.getElementById("locationName");
+var hightemp = document.getElementById("highTemp");
+var lowtemp = document.getElementById("lowTemp");
+var currenttemp = document.getElementById("curTemp");
 
 function getWeather(location) {
     var apiKey = "6e06812aeffe12f5ac01e38127f2c4fe";
@@ -27,20 +31,99 @@ function getWeather(location) {
             .then((data) => {
                 var resultObj = data;
                 return displayOutput(resultObj);
-            })        
+            })
     }
-    return displayOutput(resultObj);
+    //return displayOutput(resultObj);
 }
 
 function displayOutput(resultObj) {
 
-    let locationname = document.getElementById("locationName");
-    let hightemp = document.getElementById("highTemp");
-    let lowtemp = document.getElementById("lowTemp");
+    // let locationname = document.getElementById("locationName");
+    // let hightemp = document.getElementById("highTemp");
+    // let lowtemp = document.getElementById("lowTemp");
 
     locationname.innerText = resultObj.name;
     hightemp.innerText = resultObj.main.temp_max;
     lowtemp.innerText = resultObj.main.temp_min;
+    currenttemp.innerText = resultObj.main.temp;
+}
+
+function FahToCel(location) {
+
+    var apiKey = "6e06812aeffe12f5ac01e38127f2c4fe";
+    if (!isNaN(location)) {
+        locationkey = "zip";
+        //console.log("this is a zipcode");
+    } else {
+        locationkey = "q"
+    }
+
+    if (location != "") {
+
+        fetch('http://api.openweathermap.org/data/2.5/weather?' + locationkey + '=' + location + '&units=imperial&appid=' + apiKey)
+            .then(function (response) {
+                if (response.ok) {
+                    console.log(response);
+                    return response.json();
+                }
+            })
+            .then(function (result) {
+                jsonFile = result;
+                console.log(jsonFile);
+                return jsonFile;
+            })
+            .then((data) => {
+                var resultObj1 = data;
+                var max_temp = resultObj.main.temp_max;
+                var min_temp = resultObj.main.temp_max;
+                var current_temp = resultObj.main.temp;
+
+                hightemp.innerText = Math.round(max_temp * 9 / 5) + 32;
+                lowtemp.innerText = Math.round(min_temp * 9 / 5) + 32;
+                currenttemp.innerText = Math.round(current_temp * 9 / 5) + 32;
+            })
+    }
+
+    
+}
+
+function CelToFah(location) {
+
+    var apiKey = "6e06812aeffe12f5ac01e38127f2c4fe";
+    if (!isNaN(location)) {
+        locationkey = "zip";
+        //console.log("this is a zipcode");
+    } else {
+        locationkey = "q"
+    }
+
+    if (location != "") {
+
+        fetch('http://api.openweathermap.org/data/2.5/weather?' + locationkey + '=' + location + '&units=imperial&appid=' + apiKey)
+            .then(function (response) {
+                if (response.ok) {
+                    console.log(response);
+                    return response.json();
+                }
+            })
+            .then(function (result) {
+                jsonFile = result;
+                console.log(jsonFile);
+                return jsonFile;
+            })
+            .then((data) => {
+                var resultObj1 = data;
+                var max_temp = resultObj.main.temp_max;
+                var min_temp = resultObj.main.temp_max;
+                var current_temp = resultObj.main.temp;
+
+                hightemp.innerText = Math.round(max_temp - 32) * 5 / 9;
+                lowtemp.innerText = Math.round(min_temp - 32) * 5 / 9;
+                currenttemp.innerText = Math.round(current_temp - 32) * 5 / 9;
+            })
+    }
+
+    
 }
 
 
--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 var btn = document.getElementById('Form');
 var apiKey = "6e06812aeffe12f5ac01e38127f2c4fe";
 
+var tmpbtn = document.getElementById("tempbtn");
+
 var celciusBtn = document.getElementById('c');
 var fahBtn = document.getElementById('f');
+
+var locationname = document.getElementById("locationName");
+var hightemp = document.getElementById("highTemp");
+var lowtemp = document.getElementById("lowTemp");
+var currenttemp = document.getElementById("curTemp");
+
+
 
 btn.addEventListener('click', function (event) {
     event.preventDefault();
@@ -16,50 +25,103 @@ btn.addEventListener('click', function (event) {
     // }
 
     // if (location != "") {
-        // Used the OpenWeathermap for API - https://openweathermap.org/current
-        // fetch('http://api.openweathermap.org/data/2.5/weather?' + locationkey + '=' + location + '&units=imperial&appid=' + apiKey)
-        //     .then(function (response) {
-        //         if (response.ok) {
-        //             console.log(response);
-        //             return response.json();
-        //         }
-        //     })
-        //     .then(function (result) {
-        //         jsonFile = result;
-        //         console.log(jsonFile);
-        //         return jsonFile;
-        //     })
-        //     .then((data)=> {
-        //         var resultObj = data;
-        //         return displayOutput(resultObj);
-                
-        //     })
+    // Used the OpenWeathermap for API - https://openweathermap.org/current
+    // fetch('http://api.openweathermap.org/data/2.5/weather?' + locationkey + '=' + location + '&units=imperial&appid=' + apiKey)
+    //     .then(function (response) {
+    //         if (response.ok) {
+    //             console.log(response);
+    //             return response.json();
+    //         }
+    //     })
+    //     .then(function (result) {
+    //         jsonFile = result;
+    //         console.log(jsonFile);
+    //         return jsonFile;
+    //     })
+    //     .then((data)=> {
+    //         var resultObj = data;
+    //         return displayOutput(resultObj);
+
+    //     })
     return getWeather(location);
-            
-            
-    
 
- 
-//   .then(response => {
-//         console.log(response);
-//     })
-//         .catch(err => {
-//             console.error(err);
-//         });
 
-// function displayOutput(resultObj){
 
-//     let locationname = document.getElementById("locationName");
-//     let hightemp = document.getElementById("highTemp");
-//     let lowtemp = document.getElementById("lowTemp");
 
-//     locationname.innerText = resultObj.name;
-//     hightemp.innerText = resultObj.main.temp_max;
-//     lowtemp.innerText = resultObj.main.temp_min;
-// }
+
+    //   .then(response => {
+    //         console.log(response);
+    //     })
+    //         .catch(err => {
+    //             console.error(err);
+    //         });
+
+    // function displayOutput(resultObj){
+
+    //     let locationname = document.getElementById("locationName");
+    //     let hightemp = document.getElementById("highTemp");
+    //     let lowtemp = document.getElementById("lowTemp");
+
+    //     locationname.innerText = resultObj.name;
+    //     hightemp.innerText = resultObj.main.temp_max;
+    //     lowtemp.innerText = resultObj.main.temp_min;
+    // }
 
 });
 
+
+// tempbtn.addEventListener('submit', function (event) {
+//     event.preventDefault();
+
+//     var max_temp = resultObj.main.temp_max;
+//     var min_temp = resultObj.main.temp_max;
+//     var current_temp = resultObj.main.temp;
+
+//     const rbtns = document.querySelectorAll('input[name="Temparature"]');
+
+//     for (const rbtn of rbtns) {
+//         if (rbtn.checked) {
+//             console.log("Celsjusz");
+//             hightemp.innerText = Math.round(max_temp * 9 / 5) + 32;
+//             lowtemp.innerText = Math.round(min_temp * 9 / 5) + 32;
+//             currenttemp.innerText = Math.round(current_temp * 9 / 5) + 32;
+//         } else {
+//             console.log("Fahrenheit");
+//             hightemp.innerText = Math.round(max_temp - 32) * 5 / 9;
+//             lowtemp.innerText = Math.round(min_temp - 32) * 5 / 9;
+//             currenttemp.innerText = Math.round(current_temp - 32) * 5 / 9;
+//         }
+
+//     }
+
+
+// });
+
+// var max_temp = resultObj.main.temp_max;
+// var min_temp = resultObj.main.temp_max;
+// var current_temp = resultObj.main.temp;
+
+document.getElementById("f").addEventListener("click", function(event){
+    event.preventDefault();
+
+    var location = document.getElementById('location').value;
+    return CelToFah(location);
+
+    // hightemp.innerText = Math.round(max_temp - 32) * 5 / 9;
+    // lowtemp.innerText = Math.round(min_temp - 32) * 5 / 9;
+    // currenttemp.innerText = Math.round(current_temp - 32) * 5 / 9;
+});
+
+document.getElementById("c").addEventListener("click", function(event){
+    event.preventDefault();
+
+    var location = document.getElementById('location').value;
+    return FahToCel(location)
+
+    // hightemp.innerText = Math.round(max_temp * 9 / 5) + 32;
+    // lowtemp.innerText = Math.round(min_temp * 9 / 5) + 32;
+    // currenttemp.innerText = Math.round(current_temp * 9 / 5) + 32;
+});
 
 
 
