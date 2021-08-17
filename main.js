47e3ab8d5641cf8ff403383e11dc7821546d7cdc
Deepthi <deepthi.gaddipati.f2jn@statefarm.com>
2021-08-17 16:41:12 -0500

    Removed the unused lines of code

--- main.js
+++ main.js
@@ -1,14 +1,7 @@
 var btn = document.getElementById('Form');
-var apiKey = "6e06812aeffe12f5ac01e38127f2c4fe";
-var tmpbtn = document.getElementById("tempbtn");
 var celciusBtn = document.getElementById('c');
 var fahBtn = document.getElementById('f');
-var locationname = document.getElementById("locationName");
-var hightemp = document.getElementById("highTemp");
-var lowtemp = document.getElementById("lowTemp");
-var currenttemp = document.getElementById("curTemp");
 var errorMessage = document.getElementById('errorname');
-var dataDisplayCheck = document.getElementById("searchOutputData").hasAttribute("hidden");
 
 btn.addEventListener('submit', function (event) {
   event.preventDefault();
